5ffe9013202ae9776853993cb44e2319c1c215e0
Deepak Chapagain <deepagain81@gmail.com>
2019-03-20 22:00:17 -0500

    minor updates: menu works: menu has no functions yet

--- ESOSApplications/fcn_synth.c
+++ ESOSApplications/fcn_synth.c
@@ -9,11 +9,16 @@
 #include "esos_menu.h"
 #include "esos_lcd44780.h"
 
-//static int32_t temperature;
-//static uint16_t u16_temperature_reading;   // in millivolts
+// Array for waveforms
+#include "stdio.h"
 
-//ESOS_USER_TASK(get_temperature);
+void configDAC(void);
+void writeDAC(uint16_t u16_x, uint16_t u16_y);
 
+volatile uint8_t u8_per, u8_amp;	// variables
+volatile uint16_t u16_per;
+
+const uint8_t au8_sinetbl[] = {127,133,139,146,152,158,164,170,176,181,187,192,198,203,208,212,217,221,225,229,233,236,239,242,244,247,249,250,252,253,253,254,254,254,253,253,252,250,249,247,244,242,239,236,233,229,225,221,217,212,208,203,198,192,187,181,176,170,164,158,152,146,139,133,127,121,115,108,102,96,90,84,78,73,67,62,56,51,46,42,37,33,29,25,21,18,15,12,10,7,5,4,2,1,1,0,0,0,1,1,2,4,5,7,10,12,15,18,21,25,29,33,37,42,46,51,56,62,67,73,78,84,90,96,102,108,115,121};
 
 /*TODO: Ctrl+F replace all instances of mm with a name that abides
 		by the coding standards.
@@ -80,7 +85,7 @@ static esos_menu_entry_t ledfp = {
 // Selecting frequency
 static esos_menu_entry_t freq = {
 	.entries[0].label = "Freq= ",
-	.entries[0].value = 50, //Default, per = 1s
+	.entries[0].value = 1, //Default, per = 1s
 	.entries[0].min = 0,
 	.entries[0].max = 100,
 };
@@ -98,16 +103,21 @@ ESOS_USER_TASK( fcn_synth ) {
 		// Display main menu until sw3 pressed
 		ESOS_TASK_WAIT_ESOS_MENU_LONGMENU(my_menu);
 
-		if (my_menu.u8_choice < 7) // 5 comes from -->
+		if (my_menu.u8_choice < 0) // must have atleast one option in menu
 			ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(err);
 		else if(my_menu.u8_choice == 0){
+
+			//Display waveform menu until sw3 pressed
+			esos_lcd44780_clearScreen();
 			ESOS_TASK_WAIT_ESOS_MENU_LONGMENU(menu_setWvform);
 
 			// Inside the wvform menu, has another menu
-			if (menu_setWvform.u8_choice < 6)
+			if (menu_setWvform.u8_choice < 0)// at least one option
 				ESOS_TASK_WAIT_ESOS_MENU_STATICMENU(err);
-			else if (menu_setWvform.u8_choice == 0){
+			else if (menu_setWvform.u8_choice > 0){
 				// Display trig wave
+				// test screen by writing 5 letters to both lines
+				
 				continue;
 			}
 			else if (menu_setWvform.u8_choice == 1){
@@ -194,3 +204,14 @@ void setLEDS(){
 	// light up 7 LEDs (external leds i guess)
 	return;
 }
+
+//************************************************************************************//
+
+// to test A A A written on screen
+// int i = 0;
+// 				for(i = 0; i < 5; i++){
+// 					esos_lcd44780_clearScreen();
+// 					esos_lcd44780_writeChar( 0, i, 'A'+ i );
+
+// 				ESOS_TASK_WAIT_TICKS(500);
+// 				}
